9b4a19092c10a708a0d0306c3edcafdb468249f2
Anish <Anish@Anish-PC>
2018-02-18 21:17:32 +0530

    Searcher

---
+++ searcher/src/main/java/org/searcher/filter/SearcherRequestFilter.java
@@ -0,0 +1,26 @@
+package org.searcher.filter;
+
+import java.util.logging.Logger;
+
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+
+import org.apache.solr.core.SolrCore;
+import org.apache.solr.servlet.SolrDispatchFilter;
+
+public class SearcherRequestFilter extends SolrDispatchFilter {
+	
+	Logger logger = Logger.getLogger(SearcherRequestFilter.class.getName());
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		logger.info("Initializing Solr Searcher.");
+		System.setProperty("solr.solr.home", "/solr");
+		System.setProperty("solr.log.dir", "/solr/log/");
+		super.init(config);
+		for (SolrCore core : super.getCores().getCores()) {
+			logger.info("Solr core:" + core.getName());
+		}
+	}
+
+}
